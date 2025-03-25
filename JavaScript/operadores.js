63b1a4a2de4a048c1cc8946a4798b6582b4652a4
Vitor Alexandre Lusni <fabricioruf@gmail.com>
2025-03-25 09:57:28 -0300

    .
    .

---
+++ JavaScript/operadores.js
@@ -0,0 +1,34 @@
+//Operador Aritméticos
+
+let n1= 4
+let n2 = 2
+
+//Operador soma
+let soma =n1 +n2 
+console.log(soma);
+
+//Operador Subtração
+
+let subtracao = n1- n2
+console.log(subtracao)
+
+//operador Multiplicação
+let Multiplicação = n1 * n2
+console.log(Multiplicação)
+
+//Operador Divisão
+
+let divisão = n1 / n2
+console.log(divisão)
+
+// Operador Modulo
+let Modulo = n1 % n2
+console.log(Modulo)
+
+//Operador de Potencia
+let potencia=Math.pow(n1,n2)
+console.log(potencia)
+
+//Operador Raiz Quadrada
+let raizQuadrada = math.srqt(n1)
+console.log(raizQuadrada);
---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+//VAR e LET são variaveis
+//Declarando var
+var texto ="Hello World"
+console.log(texto) //print 
+
+//Declarando Let
+let nome = "Fabricio Gomes"
+console.log("Nome: ", nome)
+
+//Atribuindo um novo valor
+nome= "Cirillo"
+console.log("Nome: " , nome)
+
+//Atribuindo um novo valor - VAR
+texto="Olá,Mundo"
+console.log(texto) //print 
+
+//Constantes - const
+const valorPi=3.14
+console.log("O valor de PI é Igual a ",valorPi);
+
+var nomecompleto
