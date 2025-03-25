5f89cc63e6b89df6a87b6493a4756c396bdd62f7
Vitor Alexandre Lusni <fabricioruf@gmail.com>
2025-03-25 10:43:03 -0300

    .
    .

---
+++ JavaScript/Manipulando HTML/script.js
@@ -0,0 +1,20 @@
+//Mudar a cor da tela de fundo
+window.document.body.style.background="black"
+
+
+//mudar a cor da letra
+window.document.body.style.color= "white"
+
+//criando um elemento no HTML
+window.document.body.innerHTML+= "<h1>Brasil x Argentina </h1>"
+
+//Exibindo um alerto
+window.alert("Ola mundo")
+
+//Exibir uma tela de confirmação 
+var continuar =window.prompt("Você deseja continuar? ")
+window.alert(continuar)
+
+//Exibindo uma tela de confirmação
+var continuar = window.confirm("Voce deseja continuar ?")
+window.alert(continuar)
